b4f34f52a465ae24c17d92ac409ee5a9584a6541
roach <tmdgusya@naver.com>
2021-12-09 19:03:32 +0900

    init

---
+++ src/main/kotlin/open/tistory/tistoryopenapi/TistoryOpenApiApplication.kt
@@ -0,0 +1,11 @@
+package open.tistory.tistoryopenapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TistoryOpenApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TistoryOpenApiApplication>(*args)
+}
---
+++ src/test/kotlin/open/tistory/tistoryopenapi/TistoryOpenApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package open.tistory.tistoryopenapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TistoryOpenApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
