a869b987a0d6f9fb866006eac00f0dd733c30551
roach <tmdgusya@naver.com>
2021-12-11 20:51:29 +0900

    [TEST] 외부 API 호출 잘 되는지 확인

---
+++ src/test/kotlin/open/tistory/tistoryopenapi/adapter/external/ExternalApiAdaptorTest.kt
@@ -0,0 +1,31 @@
+package open.tistory.tistoryopenapi.adapter.external
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders
+
+@SpringBootTest
+internal class ExternalApiAdaptorTest(
+
+) {
+
+    @Autowired
+    lateinit var externalApiAdaptor: ExternalApiAdaptor<String>
+
+    val externalURL = "https://www.naver.com"
+
+    @Test
+    @DisplayName("외부 API 요청에 대한 GET 처리가 가능한지 확인합니다.")
+    fun testGetMethod() {
+        val result = externalApiAdaptor.get(externalURL, clazz = String::class.java)
+
+        assertNotNull(result);
+        assertNotNull(result.body);
+    }
+
+}
